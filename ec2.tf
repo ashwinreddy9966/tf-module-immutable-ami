426477a0372ec0d9a9116bac52147a74bb25fa48
manu <manu@local.com>
2022-06-11 20:24:18 +0530

    Auto Push Commit

---
+++ ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "ami-instance" {
+  ami                     = data.aws_ami.ami.id
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = [aws_security_group.allows_ssh.id]
+  iam_instance_profile    = "arn:aws:iam::834725375088:instance-profile/dev_instance_profile"
+}
+
+resource "null_resource" "app-deploy" {
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_instance.ami-instance.public_ip
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
+    }
+    inline = [
+      "ansible-pull -U https://github.com/ashwinreddy9966/ansible roboshop-pull.yml -e ENV=ENV -e APP_VERSION=${var.APP_VERSION} -e COMPONENT=${var.COMPONENT} -e MONGODB_ENDPOINT=MONGODB_ENDPOINT"
+    ]
+  }
+}
