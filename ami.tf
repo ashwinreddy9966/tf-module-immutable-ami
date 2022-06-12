ca00ecb4e8f29113c3be817173fca00d8528f683
manu <manu@local.com>
2022-06-12 07:29:56 +0530

    Auto Push Commit

--- ami.tf
+++ ami.tf
@@ -3,9 +3,9 @@ resource "aws_ami_from_instance" "ami" {
   source_instance_id = aws_instance.ami-instance.id
   depends_on         = [null_resource.app-deploy]
 
-  tags {
-    Name       = "${var.COMPONENT}-${var.APP_VERSION}"
-    COMPONENT  =
-    APP_VERSON =          
+  tags  = {
+    Name        = "${var.COMPONENT}-${var.APP_VERSION}"
+    COMPONENT   = ${var.COMPONENT}
+    APP_VERSION = var.APP_VERSION
   }
 }
